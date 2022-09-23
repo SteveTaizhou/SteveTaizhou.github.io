01c37159e29cf8c2bd2d8055b991827e0e0f5581
SteveTaizhou <85382878+SteveTaizhou@users.noreply.github.com>
2022-09-23 18:00:27 +0800

    Update gitalkg.js

--- js/gitalkg.js
+++ js/gitalkg.js
@@ -1,6 +1,6 @@
 var gitalk = new Gitalk({
-  clientID: '4cd6e43662bad7fefe96', // GitHub Application Client ID
-  clientSecret: '5b438157b509585bb60bb57fe3687b59716afd77', // GitHub Application Client Secret
+  clientID: '830ef52cb61a8719455f', // GitHub Application Client ID
+  clientSecret: 'b539c6117355b6ceeb52669a805e9d4387c068ff', // GitHub Application Client Secret
   repo: 'steveblog-gitalk-contain',      // 存放评论的仓库
   owner: 'SteveTaizhou',          // 仓库的创建者，
   admin: ['SteveTaizhou'],        // 如果仓库有多个人可以操作，那么在这里以数组形式写出
