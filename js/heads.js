6cf70f450110d2e4b7d4427c36774d5b1f089a16
SteveTaizhou <85382878+SteveTaizhou@users.noreply.github.com>
2022-12-25 18:09:45 +0800

    Update heads.js

--- js/heads.js
+++ js/heads.js
@@ -1,10 +1,11 @@
-var obj=document.getElementById("headers")
-obj.innerHTML=`<nav>
+var obj = document.getElementById("headers")
+obj.innerHTML = `<nav>
 	  <p>Steve的小工具网站</p>
 	  <ul class="header-ul">
 	    <li><a href="/">首页</a></li>
 	    <li><a href="#top">顶部</a></li>
 	    <li><a href="..">返回</a></li>
 	    <li><a href="./">索引</a></li>
+	    <li><a href="/christmastree.html">圣诞树</a></li>
 	  </ul>
 	</nav>`
